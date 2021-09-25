e6ae9549642b2d3349e62f066c2ad963d16e53b2
naalmeida98 <natii_98_@hotmail.com>
2021-09-25 12:50:51 -0300

    Atualização

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //define a minha rota
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() { //meu primeiro evento
+                EventoId = 1,
+                Local = "SB",
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                Tema = "Civil",
+                QtdPessoas = 200,
+                Lote = "1542",
+                ImagemURL = "foto.png"
+            }, 
+            new Evento() { //meu primeiro evento
+                EventoId = 2,
+                Local = "SB",
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                Tema = "Casamento",
+                QtdPessoas = 200,
+                Lote = "1542",
+                ImagemURL = "foto2.png"
+            } 
+        };
+        
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get() //retornar um array
+        {
+            return _evento; 
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id) //retornar um array
+        {
+            return _evento.Where(evento => evento.EventoId == id); 
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "testando Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"testando Put com id: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"testando Delete com id: {id}";
+        }
+    }
+}
---
+++ Back/src/ProEventos.API/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace ProEventos.API
+{
+    public class Startup
+    {
+        // IConfiguration está sendo injetado
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllers(); //arquitetura mvc //**
+            services.AddSwaggerGen(c => //trabalha por meio de versão
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                //abaixo temos o contrato
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProEventos.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting(); //**
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => //**
+            {
+                endpoints.MapControllers();
+            });
+
+            //**
+            //o controller será chamado quando o uso de determinada rota (app.UseRouting();) 
+            //te retornar determinado endpoint
+        }
+    }
+}
