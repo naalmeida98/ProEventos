7fcddad9652acdf29ccb52298dce88461ce864ab
naalmeida98 <natii_98_@hotmail.com>
2021-09-27 21:38:13 -0300

    Angular

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -7,19 +7,25 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent, //declara esse componente
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
   imports: [ //importa esses módulos
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule //auxilia na referência dentro do componente
+    BrowserAnimationsModule, //auxilia na referência dentro do componente
+    CollapseModule.forRoot(), //para estilizar paginas com animações
+    FormsModule //para usar com binding do tipo [()]
   ],
   providers: [],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  widthImg:number = 150;
+  marginImg:number = 2;
+  openImg:boolean = true;
+  private _filtroLista: string = '';
 
   constructor(private http: HttpClient) { } //faz a declaração do http client para o BD
 
@@ -18,11 +22,35 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void { //integração com BD
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-
+    response => {
+      this.eventos = response;
+    },
+    error => console.log(error)
     ); //me inscrevo nesse http
   }
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventos = this.filtroLista ? this.filtrarEventos(this._filtroLista) : this.getEventos();
+  }
+
+  public filtrarEventos(filtro: string): any{
+    filtro = filtro.toLocaleLowerCase(); //coloco tudo em minúsculo
+    return this.eventos.filter(
+      (      evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtro) !== -1
+    );
+  }
+
+  public clickImg(): void{
+    this.openImg = !this.openImg; //toda vez que clicar no button de mostrar imagem, o seu estado
+    //será modificado
+  }
+
+
 
 
 
